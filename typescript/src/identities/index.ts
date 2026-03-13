42e822d807d155a81511f9899024fc85e733aa69
Ray Liao <17989965+rayruizhiliao@users.noreply.github.com>
2026-03-13 11:24:56 -0400

    rm unused code

--- typescript/src/identities/index.ts
+++
@@ -1,7 +0,0 @@
-export { InkboxIdentities } from "./client.js";
-export type {
-  AgentIdentity,
-  AgentIdentityDetail,
-  IdentityMailbox,
-  IdentityPhoneNumber,
-} from "./types.js";
--- typescript/src/phone/index.ts
+++
@@ -1,8 +0,0 @@
-export type { SigningKey } from "../resources/signing-keys.js";
-export type {
-  PhoneNumber,
-  PhoneCall,
-  PhoneCallWithRateLimit,
-  RateLimitInfo,
-  PhoneTranscript,
-} from "./types.js";
